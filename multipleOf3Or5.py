5f6a605e0c8cd66d380697dfe7b882de36d8cd4c
Graham Patton <graham.m.patton@gmail.com>
2022-07-16 19:58:59 -0400

    6 kyu multiples of 3 or 5 solution

---
+++ multipleOf3Or5.py
@@ -0,0 +1,21 @@
+# Multiples of 3 or 5
+
+# 6 kyu 
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+# Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(number):
+    
+    sum = 0
+    
+    if number < 0:
+        return 0
+    else:
+        for num in range(number):
+            if num % 3 == 0 or num % 5 == 0:
+                sum += num
+        return sum
